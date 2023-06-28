8df39fa19b5aafbe7e14fc46c18c1ec6484fc463
Bomby K <bomby@flyntlok.com>
2023-06-27 23:33:41 -0800

    pass

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,6 +18,7 @@ export default function Home() {
       <div className="grid text-center">
 
         <Link
+          passHref
           href="/calendar"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
         >
