3b8b2317481d32e8d21ecc5051a4c6eaf2e74568
Bomby K <bomby@flyntlok.com>
2023-06-27 23:24:42 -0800

    bah

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Home() {
   return (
@@ -16,7 +17,7 @@ export default function Home() {
 
       <div className="grid text-center">
 
-        <a
+        <Link
           href="/calendar"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
         >
@@ -29,7 +30,7 @@ export default function Home() {
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
             Tournaments, meetups, and more.
           </p>
-        </a>
+        </Link>
 
         <a
           href="https://twitch.tv/alaskagamers" className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
