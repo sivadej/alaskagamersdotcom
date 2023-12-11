6a2b889bac21d77cb95961c8da78cd01b1b970d8
Bomby <bomby@flyntlok.com>
2023-12-10 16:52:02 -0900

    trying server component

---
+++ src/app/challonge.tsx
@@ -0,0 +1,68 @@
+const LIST_LIMIT = 10;
+const apiKey = process.env.CHALLONGE_API_KEY ?? '';
+
+export default async function Challonge() {
+  const brackets = await fetchBrackets();
+  return (
+    <div className='group text-center rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-neutral-700 hover:bg-neutral-800/30'>
+      <h2 className={`mb-1 text-2xl font-semibold`}>Latest Brackets</h2>
+        <p className={`m-0 text-sm opacity-50`}>
+          Retrieved just now from Challonge:
+        </p>
+        <div>
+          {brackets.map((bracket: any) => {
+            const { name, started_at, id, full_challonge_url, sign_up_url } =
+              bracket ?? {};
+            const signUpUrl = sign_up_url && !started_at ? sign_up_url : null;
+            return (
+              <div
+                key={id}
+                className='mt-3 text-blue-300 underline text-lg hover:text-gray-400'
+              >
+                <a href={signUpUrl ?? full_challonge_url}>{name}</a>
+              </div>
+            );
+          })}
+        </div>
+    </div>
+  );
+}
+
+async function fetchBrackets() {
+  const today = new Date();
+  const ninetyDaysAgo = new Date(today.setDate(today.getDate() - 30));
+  const createdAfterDate = ninetyDaysAgo.toISOString().slice(0, 10);
+
+  const fetchStr = `https://api.challonge.com/v1/tournaments.json?api_key=${apiKey}&state=all&subdomain=akg&created_after=${createdAfterDate}`;
+  const response = await fetch(fetchStr);
+  const data = await response.json();
+
+  if (!data || !Array.isArray(data)) return [];
+
+  const ret = [];
+  for (let i = 0; i < data.length; i++) {
+    const { tournament } = data[i] ?? {};
+    if (!tournament) break;
+    const {
+      name,
+      full_challonge_url,
+      sign_up_url,
+      url,
+      started_at,
+      completed_at,
+      game_name,
+      participants_count,
+      id,
+    } = tournament;
+    ret.push({
+      name,
+      started_at,
+      id,
+      full_challonge_url,
+      sign_up_url,
+    });
+  }
+  ret.sort((a, b) => b.id - a.id);
+
+  return ret.slice(0, LIST_LIMIT);
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,9 @@
-"use client";
 import HomeLogo from "@/components/HomeLogo";
-import Image from "next/image";
 import Link from "next/link";
-import { useState, useEffect } from "react";
+import Challonge from './challonge';
+import { Suspense } from "react";
 
 export default function Home() {
-  const [brackets, setBrackets] = useState([]);
-  const [dataLoaded, setDataLoaded] = useState(false);
-
-  useEffect(() => {
-    async function fetchBrackets() {
-      const response = await fetch("/api/latest-brackets");
-      const data = await response.json();
-      setBrackets(data);
-      setDataLoaded(true);
-    }
-
-    fetchBrackets();
-  }, []);
 
   return (
     <main className="flex min-h-screen flex-col items-center p-8">
@@ -33,13 +19,13 @@ export default function Home() {
           href="/calendar"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-neutral-700 hover:bg-neutral-800/30"
         >
-          <h2 className={`mb-1 text-2xl font-semibold`}>
+          <h2 className="mb-1 text-2xl font-semibold">
             Events{" "}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
           </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+          <p className="m-0 max-w-[30ch] text-sm opacity-50">
             Tournaments, meetups, and more.
           </p>
         </Link>
@@ -92,45 +78,73 @@ export default function Home() {
           </h2>
         </a>
 
-        {/* <hr className="my-8 border-gray-100 opacity-30" />
-        <a
-          href="https://docs.google.com/spreadsheets/d/e/2PACX-1vQz3EmwZjw3en822pIzMlbMeo8OC-TqrnVAgt2MIGvxWSkHuaur95R6CHPAg5qmQmOW6qsh24wl7IUk/pubhtml"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-neutral-700 hover:bg-neutral-800/30"
-        >
-          <h2 className={`mb-1 text-2xl font-semibold`}>
-            Standings{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Ranbats rankings.
-          </p>
-        </a> */}
         <hr className="my-8 border-gray-100 opacity-30" />
       </div>
-      <div className="group text-center rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-neutral-700 hover:bg-neutral-800/30">
-        <h2 className={`mb-1 text-2xl font-semibold`}>Latest Brackets</h2>
-        <p className={`m-0 text-sm opacity-50`}>
-          {dataLoaded ? "Retrieved just now from Challonge:" : "Loading recent tournaments..."}
-        </p>
-        <div>
-          {brackets.map((bracket: any) => {
-            const { name, started_at, id, full_challonge_url, sign_up_url } =
-              bracket ?? {};
-            const signUpUrl = sign_up_url && !started_at ? sign_up_url : null;
-            return (
-              <div key={id} className="mt-3 text-blue-300 underline text-lg">
-                <a href={signUpUrl ?? full_challonge_url}>{name}</a>
-              </div>
-            );
-          })}
-        </div>
-      </div>
-      <hr className="my-8 border-gray-100 opacity-30" />
 
+      <Suspense fallback={<SkeletonLoader />}>
+        <Challonge />
+      </Suspense>
+
+      <hr className="my-8 border-gray-100 opacity-30" />
       <div className="flex items-center space-x-4">
-        <a
+        <TwitterLink />
+        <InstagramLink />
+      </div>
+    </main>
+  );
+}
+
+function SkeletonLoader() {
+  return (
+    <div className="group text-center rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-neutral-700 hover:bg-neutral-800/30">
+      <h2 className={`mb-1 text-2xl font-semibold`}>Latest Brackets</h2>
+      <p className={`m-0 text-sm opacity-50`}>
+        Getting latest from Challonge:
+      </p>
+      <div className="text-center">
+        <GlowyLine />
+        <GlowyLine />
+        <GlowyLine />
+        <GlowyLine />
+        <GlowyLine />
+        <GlowyLine />
+      </div>
+    </div>
+  );
+}
+
+function GlowyLine () {
+  const randomWidth = Math.floor(Math.random() * 100) + 300;
+  return <div className="text-center bg-blue-300 animate-pulse" style={{ height: 18, width: randomWidth, margin: 'auto', marginTop: 12 }} />
+}
+
+function InstagramLink() {
+  return (
+    <a
+      href="https://www.instagram.com/alaskagamers"
+      className="text-gray-400 hover:text-pink-400"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M20 6.5h-3.6l-1.9-2.5h-5.2L7.6 6.5H4a1.5 1.5 0 0 0-1.5 1.5v9A1.5 1.5 0 0 0 4 18.5h16a1.5 1.5 0 0 0 1.5-1.5v-9a1.5 1.5 0 0 0-1.5-1.5zM12 15a3 3 0 1 1 0-6 3 3 0 0 1 0 6z"
+            />
+          </svg>
+        </a>
+  );
+}
+
+function TwitterLink() {
+  return (
+    <a
           href="https://twitter.com/alaskagamers"
           className="text-gray-400 hover:text-blue-400"
         >
@@ -149,26 +163,5 @@ export default function Home() {
             />
           </svg>
         </a>
-        <a
-          href="https://www.instagram.com/alaskagamers"
-          className="text-gray-400 hover:text-pink-400"
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-6 w-6"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth={2}
-              d="M20 6.5h-3.6l-1.9-2.5h-5.2L7.6 6.5H4a1.5 1.5 0 0 0-1.5 1.5v9A1.5 1.5 0 0 0 4 18.5h16a1.5 1.5 0 0 0 1.5-1.5v-9a1.5 1.5 0 0 0-1.5-1.5zM12 15a3 3 0 1 1 0-6 3 3 0 0 1 0 6z"
-            />
-          </svg>
-        </a>
-      </div>
-    </main>
   );
 }
