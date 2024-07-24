3ce77f1c27cd853ed878f2b5cda9bfdc761b7cac
sivadej <bombyk@gmail.com>
2024-07-23 20:56:53 -0800

    switch to static data

--- src/app/evo-tracker/common/fetchData.ts
+++ src/app/evo-tracker/common/fetchData.ts
@@ -1,31 +1,43 @@
 'use server';
 
-import { createClient } from '@supabase/supabase-js';
 import { convertPlayer } from './functions';
 import { ParticipantQueryRaw } from './startggSchemaTypes';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || '';
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY || '';
-
-const supabase = createClient(supabaseUrl, supabaseKey);
+import staticPlayerData from './staticPlayerData.json';
 
 export async function getAllPlayers() {
-  const { data } = await supabase
-    .from('players')
-    .select('id,data')
-    .neq('id', new Date().getTime() * -1); // cache bustin'
-  // console.log(JSON.stringify(data, null, 2));
-  return (data as unknown as ParticipantQueryRaw[]) ?? [];
+  return (staticPlayerData as unknown as ParticipantQueryRaw[]) ?? [];
 }
 
 export async function getPlayerById(playerId: string) {
   if (!playerId) return null;
-
-  const { data } = await supabase
-    .from('players')
-    .select('id,data')
-    .eq('id', playerId);
-  // console.log(JSON.stringify(data, null, 2));
-  const [player] = (data as unknown as ParticipantQueryRaw[]) ?? [];
+  const player = staticPlayerData.find(
+    (p) => p.id === +playerId,
+  ) as unknown as ParticipantQueryRaw;
   return convertPlayer(player?.data?.data?.participant);
 }
+
+// import { createClient } from '@supabase/supabase-js';
+// const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || '';
+// const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY || '';
+// const supabase = createClient(supabaseUrl, supabaseKey);
+
+// export async function getAllPlayers_LIVE() {
+//   const { data } = await supabase
+//     .from('players')
+//     .select('id,data')
+//     .neq('id', new Date().getTime() * -1); // cache bustin'
+//   console.log(JSON.stringify(data, null, 2));
+//   return (data as unknown as ParticipantQueryRaw[]) ?? [];
+// }
+
+// export async function getPlayerById_LIVE(playerId: string) {
+//   if (!playerId) return null;
+//   const { data } = await supabase
+//     .from('players')
+//     .select('id,data')
+//     .eq('id', playerId);
+//   // console.log(JSON.stringify(data, null, 2));
+//   const [player] = (data as unknown as ParticipantQueryRaw[]) ?? [];
+//   return convertPlayer(player?.data?.data?.participant);
+// }
