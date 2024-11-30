c5491615addb9b0893358bdf06e7210ca1343b68
sivadej <bombyk@gmail.com>
2024-11-29 15:48:41 -0900

    twilio wip

---
+++ src/app/api/reminders-signup/route.ts
@@ -0,0 +1,76 @@
+import { createClient } from '@supabase/supabase-js';
+import { NextRequest, NextResponse } from 'next/server';
+import twilio from 'twilio';
+
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || '';
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY || '';
+const supabaseClient = createClient(supabaseUrl, supabaseKey);
+
+const accountSid = process.env.TWILIO_TEST_ACCOUNT_SID;
+const authToken = process.env.TWILIO_TEST_AUTH_TOKEN;
+const twilioPhoneNumber = process.env.TWILIO_TEST_PHONE_NUMBER;
+const twilioClient = twilio(accountSid, authToken);
+
+export async function POST(req: NextRequest) {
+  try {
+    const { phoneNumber } = await req.json();
+
+    if (!phoneNumber) {
+      return NextResponse.json(
+        { error: 'Phone number is required' },
+        { status: 400 },
+      );
+    }
+
+    let twilioResponse;
+    // Send confirmation message using Twilio
+    const messageBody = {
+      body: 'You are now receiving AlaskaGamers event reminders! Reply STOP at any time to opt out.',
+      from: twilioPhoneNumber, // Your Twilio phone number
+      to: phoneNumber,
+      // to: '+15005550006', // Twilio test number fail
+      // to: '+15005550001', // Twilio test number success
+    };
+    try {
+      twilioResponse = await twilioClient.messages.create(messageBody);
+    } catch (twilioError) {
+      console.error('Error sending Twilio message:', twilioError);
+      twilioResponse = twilioError;
+    }
+
+    // Log the Twilio API request and response
+    const { error: logError } = await supabaseClient
+      .from('twilio_logs')
+      .insert([
+        {
+          api_request: messageBody,
+          api_response: twilioResponse,
+        },
+      ]);
+
+    if (logError) {
+      console.error('Error logging Twilio API request/response:', logError);
+    }
+
+    const { error: dbError } = await supabaseClient.from('sms_capture').insert([
+      {
+        phone_number: phoneNumber,
+        // confirmation_sent: res?.
+      },
+    ]);
+
+    if (dbError) {
+      throw dbError;
+    }
+
+    return NextResponse.json(
+      { message: 'Phone number saved successfully' },
+      { status: 200 },
+    );
+  } catch (error) {
+    return NextResponse.json(
+      { error: (error as Error).message },
+      { status: 500 },
+    );
+  }
+}
--- src/app/jinglebellbrawl/page.tsx
+++ src/app/jinglebellbrawl/page.tsx
@@ -1,6 +1,115 @@
+'use client';
 import Image from 'next/image';
+import { useState } from 'react';
 
-export default async function Page() {
+async function SignUpForm() {
+  const [phoneNumber, setPhoneNumber] = useState('');
+  const [message, setMessage] = useState('');
+  const [prevValue, setPrevValue] = useState('');
+  const [status, setStatus] = useState<
+    'INIT' | 'SUBMITTING' | 'SUCCESS' | 'ERROR'
+  >('INIT');
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    setStatus('SUBMITTING');
+
+    try {
+      const response = await fetch('/api/reminders-signup', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ phoneNumber }),
+      });
+
+      console.log(response);
+      console.log('um', response.status);
+
+      const data = await response.json();
+      // console.log(data);
+
+      if (response.ok) {
+        setMessage('Phone number saved successfully');
+        setStatus('SUCCESS');
+      } else {
+        throw new Error();
+      }
+    } catch (error) {
+      setMessage('An error occurred. Please try again.');
+      setStatus('ERROR');
+    }
+  };
+
+  const formatPhoneNumber = (value: string) => {
+    if (!value) return value;
+
+    const phoneNumber = value.replace(/[^\d]/g, '');
+    const phoneNumberLength = phoneNumber.length;
+
+    if (phoneNumberLength < 4) return phoneNumber;
+    if (phoneNumberLength < 7) {
+      return `(${phoneNumber.slice(0, 3)}) ${phoneNumber.slice(3)}`;
+    }
+    return `(${phoneNumber.slice(0, 3)}) ${phoneNumber.slice(3, 6)}-${phoneNumber.slice(6, 10)}`;
+  };
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (
+      e.target.value === '1' ||
+      e.target.value === '0' ||
+      e.target.value === '+'
+    ) {
+      return;
+    }
+
+    const input = e.target.value.replace(/[^\d]/g, '');
+    const isBackspace = e.target.value.length < prevValue.length;
+
+    if (isBackspace && input.length < phoneNumber.length) {
+      setPhoneNumber(input);
+    } else if (input.length <= 10) {
+      setPhoneNumber(input);
+    }
+
+    setPrevValue(e.target.value);
+  };
+
+  return (
+    <>
+      {status === 'SUCCESS' ? (
+        <div>
+          Thanks! You&apos;ll receive texts about Jingle Bell Brawl and other
+          AlaskaGamers events!
+        </div>
+      ) : (
+        <>
+          <h3>Enter your phone number to receive text updates:</h3>
+          <form onSubmit={handleSubmit} className="mt-2 m-auto">
+            <input
+              type="tel"
+              value={formatPhoneNumber(phoneNumber)}
+              onChange={handleInputChange}
+              placeholder="(907) 555-1234"
+              className="bg-gray-800 p-2 mb-1 w-40"
+              disabled={status === 'SUBMITTING'}
+            />
+            <button type="submit" className="bg-blue-500 text-white p-2 ml-1">
+              {status === 'SUBMITTING' ? 'Submitting...' : 'Submit'}
+            </button>
+          </form>
+          <div className="text-gray-400 text-sm">
+            By entering your phone number, you agree to receive text messages
+            from AlaskaGamers. Message and data rates may apply. Reply STOP at
+            any time to unsubscribe.
+          </div>
+        </>
+      )}
+    </>
+  );
+}
+
+export default function Page() {
   return (
     <div className="m-auto text-center mt-8">
       <Image
@@ -10,10 +119,14 @@ export default async function Page() {
         height={965}
         className="inline"
       />
-      <h1 className="mt-8 mb-12 text-2xl">More info coming soon</h1>
-      <a href="/" className="text-blue-500 p-8">
-        Return to Homepage
-      </a>
+      <h1 className="mt-8 mb-12 text-2xl">More info coming soon!</h1>
+      {process.env.NODE_ENV === 'development' ? <SignUpForm /> : null}
+
+      <div className="mt-12">
+        <a href="/" className="text-blue-500">
+          Return to Homepage
+        </a>
+      </div>
     </div>
   );
 }
